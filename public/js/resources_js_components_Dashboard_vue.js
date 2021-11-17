be28dd8ac8b310b0810dc3bb7eae750824bcebf0
tiosaputro <adhityasaputra39@Gmail.com>
2021-11-17 11:23:10 +0700

    Uplaod File

--- public/js/resources_js_components_Dashboard_vue.js
+++ public/js/resources_js_components_Dashboard_vue.js
@@ -11,17 +11,8 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "render": () => (/* binding */ render)
 /* harmony export */ });
-/* harmony import */ var vue__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! vue */ "./node_modules/vue/dist/vue.esm-bundler.js");
-
-var _hoisted_1 = {
-  "class": "grid"
-};
-
-var _hoisted_2 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createStaticVNode)("<div class=\"col-12 lg:col-6 xl:col-3\"><div class=\"card mb-0\"><div class=\"flex justify-content-between mb-3\"><div><span class=\"block text-500 font-medium mb-3\">Orders</span><div class=\"text-900 font-medium text-xl\">152</div></div><div class=\"flex align-items-center justify-content-center bg-blue-100 border-round\" style=\"width:2.5rem;height:2.5rem;\"><i class=\"pi pi-shopping-cart text-blue-500 text-xl\"></i></div></div><span class=\"text-green-500 font-medium\">24 new </span><span class=\"text-500\">since last visit</span></div></div>", 1);
-
-var _hoisted_3 = [_hoisted_2];
 function render(_ctx, _cache) {
-  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_1, _hoisted_3);
+  return null;
 }
 
 /***/ }),
